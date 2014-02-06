e1d80fdc114429fe8029afcedfe6e37dd4cf13e4
Joey Lawrance <lawrancej@wit.edu>
2014-02-06 11:24:00 -0500

    Added examples of visitors, tree traversal and search.

--- workspace/Examples/src/main/java/edu/wit/comp310/lab2/MyBSTMap.java
+++ workspace/Examples/src/main/java/edu/wit/comp310/lab2/MyBSTMap.java
@@ -1,6 +1,7 @@
 package edu.wit.comp310.lab2;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -25,6 +26,112 @@ public class MyBSTMap<Key extends Comparable<Key>,Value> implements Map<Key, Val
 	public interface Visitor<T> {
 		public void visit(BinaryTreeNode<T> node);
 	}
+	public static void main(String[] args) {
+		BinaryTreeNode<String> example = new BinaryTreeNode<String>();
+		example.data = "Luke";
+		example.left = new BinaryTreeNode<String>();
+		example.left.data = "Jian";
+		example.right = new BinaryTreeNode<String>();
+		example.right.data = "Pat";
+		example.left.left = new BinaryTreeNode<String>();
+		example.left.left.data = "Alex";
+		example.left.right = new BinaryTreeNode<String>();
+		example.left.right.data = "Kyle";
+		
+		// example.accept(new PostfixPrinter());
+		
+		Counter<String> c = new Counter<String>();
+		example.accept(c);
+		// System.out.println(c.count);
+		
+		Searcher searcher = new Searcher("Luke");
+		example.accept(searcher);
+		System.out.println(searcher.found); // print true, please!
+	}
+	public static class Searcher implements Visitor<String> {
+		String needle;
+		boolean found = false;
+		public Searcher(String needle) {
+			this.needle = needle;
+		}
+		@Override
+		public void visit(BinaryTreeNode<String> node) {
+			int result = node.data.compareTo(needle);
+			if (result < 0) { // data < needle
+				// Visit the right side
+				if (node.right != null) {
+					node.right.accept(this);
+				}
+			} else if (result == 0) { // data == needle
+				found = true;
+			} else { // data > needle
+				// Visit the left side
+				if (node.left != null) {
+					node.left.accept(this);
+				}
+			}
+		}
+	}
+	public static class Counter<T> implements Visitor<T> {
+		int count = 0;
+		@Override
+		public void visit(BinaryTreeNode<T> node) {
+			count++;
+			// Visit the left side
+			if (node.left != null) {
+				node.left.accept(this);
+			}
+			// Visit the right side
+			if (node.right != null) {
+				node.right.accept(this);
+			}
+		}
+	}
+	public static class PrefixPrinter implements Visitor<String> {
+		@Override
+		public void visit(BinaryTreeNode<String> node) {
+			// Process the node
+			System.out.println(node.data);
+			// Visit the left side
+			if (node.left != null) {
+				node.left.accept(this);
+			}
+			// Visit the right side
+			if (node.right != null) {
+				node.right.accept(this);
+			}
+		}
+	}
+	public static class InfixPrinter implements Visitor<String> {
+		@Override
+		public void visit(BinaryTreeNode<String> node) {
+			// Visit the left side
+			if (node.left != null) {
+				node.left.accept(this);
+			}
+			// Process the node
+			System.out.println(node.data);
+			// Visit the right side
+			if (node.right != null) {
+				node.right.accept(this);
+			}
+		}
+	}
+	public static class PostfixPrinter implements Visitor<String> {
+		@Override
+		public void visit(BinaryTreeNode<String> node) {
+			// Visit the left side
+			if (node.left != null) {
+				node.left.accept(this);
+			}
+			// Visit the right side
+			if (node.right != null) {
+				node.right.accept(this);
+			}
+			// Process the node
+			System.out.println(node.data);
+		}
+	}
 
 	@Override
 	public void clear() {
@@ -46,7 +153,8 @@ public class MyBSTMap<Key extends Comparable<Key>,Value> implements Map<Key, Val
 
 	@Override
 	public Set<java.util.Map.Entry<Key, Value>> entrySet() {
-		// TODO Auto-generated method stub
+		Set<Pair<Key, Value>> set = new HashSet<Pair<Key, Value>>();
+		// populate the set with everything in the the tree.
 		return null;
 	}
 
@@ -64,7 +172,8 @@ public class MyBSTMap<Key extends Comparable<Key>,Value> implements Map<Key, Val
 
 	@Override
 	public Set<Key> keySet() {
-		// TODO Auto-generated method stub
+		Set<Key> set = new HashSet<Key>();
+		// populate the set with every key in the the tree.
 		return null;
 	}
 
